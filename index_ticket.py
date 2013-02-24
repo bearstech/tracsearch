2bc44b79b5f534be6d3681e00f32bf469e4836d1
Mathieu Lecarme <mlecarme@bearstech.com>
2013-02-25 00:34:35 +0100

    Bulk is better.

--- index_ticket.py
+++ index_ticket.py
@@ -9,9 +9,10 @@ trac = Trac()
 search = Search()
 search.recreate()
 
-week = datetime.timedelta(7)
+week = datetime.timedelta(15)
 for ticket, comments in trac.ticket(week):
     print ticket['summary']
-    search.index(ticket, 'trac', 'ticket', id=ticket['id'])
+    search.index(ticket, 'trac', 'ticket', id=ticket['id'], bulk=True)
     for comment in comments:
-        search.index(comment, 'trac', 'comment', parent=ticket['id'])
+        search.index(comment, 'trac', 'comment', parent=ticket['id'], bulk=True)
+search.flush()
