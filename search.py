7a3bbda7e55b1aaa424ecb2b8a998d3af4898b16
Mathieu Lecarme <mlecarme@bearstech.com>
2013-02-24 22:03:51 +0100

    Search as an object.

---
+++ search.py
@@ -0,0 +1,34 @@
+from pyes import ES
+
+
+class Search(object):
+    def __init__(self):
+        self.conn = ES('127.0.0.1:9200')
+        self.index = self.conn.index
+
+    def delete(self):
+        try:
+            self.conn.indices.delete_index("trac")
+        except:
+            pass
+
+    def create(self):
+        self.conn.indices.create_index("trac")
+        mapping = {
+            'name': {
+                'boost': 1.0,
+                'index': 'analyzed',
+                'store': 'yes',
+                'type': 'string',
+                "term_vector": "with_positions_offsets"
+            },
+        }
+        self.conn.indices.put_mapping("wiki", {'properties': mapping}, ["trac"])
+
+    def recreate(self):
+        self.delete()
+        self.create()
+
+    def flush(self):
+        self.conn.flush_bulk()
+        self.conn.indices.refresh("trac")
