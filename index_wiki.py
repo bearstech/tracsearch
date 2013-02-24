e6e83083569b0d3bf8374cd13f4db9ec71209e90
Mathieu Lecarme <mlecarme@bearstech.com>
2013-02-24 22:08:26 +0100

    Cleaning.

--- index_wiki.py
+++ index_wiki.py
@@ -1,49 +1,15 @@
 #!/usr/bin/env python
-import os
-import xmlrpclib
-from pyes import ES
 
-conn = ES('127.0.0.1:9200')
-try:
-    conn.indices.delete_index("trac")
-except:
-    pass
-conn.indices.create_index("trac")
-mapping = {
-    'name': {
-        'boost': 1.0,
-        'index': 'analyzed',
-        'store': 'yes',
-        'type': 'string',
-        "term_vector": "with_positions_offsets"
-    },
-}
-conn.indices.put_mapping("wiki", {'properties': mapping}, ["trac"])
+from trac import Trac
+from search import Search
 
+trac = Trac()
+search = Search()
 
-uri = "%s/trac/rpc" % os.environ['TRAC_URL']
-print uri
-trac = xmlrpclib.ServerProxy(uri)
-print trac.system.listMethods()
-print trac.search.getSearchFilters()
-for page in trac.wiki.getAllPages():
-    info = trac.wiki.getPageInfo(page)
-    #print dir(info['lastModified'])
-    #print info['lastModified'].value
-    print "\t", info['name']
-    try:
-        body = trac.wiki.getPageHTML(page)
-    except:
-        body = ''
-        print "Oups sur la page %s" % info['name']
-    conn.index({
-        'name': info['name'],
-        'author': info['author'],
-        'version': info['version'],
-        'lastModified': info['lastModified'].value,
-        'body': body
-    }, 'trac', 'wiki', bulk=True)
-    #print trac.wiki.getPageHTML(page)
+search.recreate()
 
-conn.flush_bulk()
-conn.indices.refresh("trac")
+for wiki in trac.wiki():
+    print wiki['name']
+    search.index(wiki, 'trac', 'wiki', id=wiki['name'], bulk=True)
+
+search.flush()
