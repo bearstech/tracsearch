d631a20c9fcba3ce066c9515acb6ace45997e4bf
Mathieu Lecarme <mlecarme@bearstech.com>
2013-03-08 17:33:01 +0100

    Bulk import.

--- index_ticket.py
+++ index_ticket.py
@@ -12,8 +12,15 @@ config.read(['tracsearch.ini'])
 trac = Trac(config.get('trac', 'url', None))
 search = Search(config.get('elasticsearch', 'url', 'http://127.0.0.1:9200'))
 
-period = datetime.timedelta(int(config.get('index:ticket', 'timedelta', 30)))
+period = datetime.timedelta(config.getint('index:ticket', 'timedelta'))
 bulk_tickets = []
+bulk_size = config.getint('elasticsearch', 'bulk_size')
+
+
+def bulk_index(search, bulk_tickets):
+    search.es.bulk_index('trac', 'ticket', bulk_tickets)
+    search.flush()
+
 for ticket, comments in trac.ticket(period):
     print ticket['summary']
     ticket['comment'] = []
@@ -23,5 +30,9 @@ for ticket, comments in trac.ticket(period):
         ticket['comment'].append(comment)
         n += 1
     bulk_tickets.append(ticket)
-search.es.bulk_index('trac', 'ticket', bulk_tickets)
-search.flush()
+    if len(bulk_tickets) >= bulk_size:
+        bulk_index(search, bulk_tickets)
+        bulk_tickets = []
+
+if len(bulk_tickets):
+    bulk_index(search, bulk_tickets)
